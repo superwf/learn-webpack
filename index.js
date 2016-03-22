c675505cc457a1c59a77daacf193965cb7c5276d
superwf <superwf@gmail.com>
2016-03-22 17:21:45 +0800

    modify karma coverage

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
 import 'babel-polyfill';
-import React from 'react';
-import {render} from 'react-dom';
-import Todo from './lib/containers/Todo';
-import {Router, hashHistory, Route} from 'react-router';
-import {Provider} from 'react-redux';
-import store from './lib/store';
+// import React from 'react';
+// import {render} from 'react-dom';
+// import Todo from './lib/containers/Todo';
+// import {Router, hashHistory, Route} from 'react-router';
+// import {Provider} from 'react-redux';
+// import store from './lib/store';
 
-let main = document.querySelector('#main');
+import d3 from 'd3';
 
-render(<Provider store={store}>
-  <Router history={hashHistory}>
-    <Route path='/' component={Todo} />
-  </Router>
-</Provider>, main);
-
-
+// let main = document.querySelector('#main');
 // render(<Provider store={store}>
-//   <Todo />
+//   <Router history={hashHistory}>
+//     <Route path='/' component={Todo} />
+//   </Router>
 // </Provider>, main);
+
+let body = d3.select('body');
+let svg = body.select('svg');
+let dataset = [250 , 210 , 170 , 130 , 90];
+let rectHeight = 25;
+
+let linear = d3.scale.linear()
+  .domain([d3.min(dataset), d3.max(dataset)])
+  .range([10, 400]);
+
+var axis = d3.svg.axis()
+  .scale(linear)
+  .orient('bottom')
+  .ticks(7);
+svg.selectAll('rect')
+  .data(dataset)
+  .enter()
+  .append('rect')
+  .attr({
+    x: 20,
+    y: (_, i) => (
+      i * rectHeight
+    ),
+    width: d => linear(d),
+    height: rectHeight - 1,
+    fill: 'steelblue'
+  });
+svg.append('g')
+.attr('transform', 'translate(20, 130)')
+.call(axis);
--- karma.conf.js
+++ karma.conf.js
@@ -66,6 +66,12 @@ module.exports = function(config) {
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['progress', 'coverage'],
+    coverageReporter: {
+      reporters: [
+        {type: 'lcov', dir: '../coverage', subdir: '.'},
+        {type: 'text-summary', dir: '../coverage', subdir: '.'}
+      ]
+    },
 
 
     // web server port
